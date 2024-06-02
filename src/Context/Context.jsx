1bf60bf31a5132e1bd3162ad0c1020b3ed1d5a10
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2024-06-02 11:25:19 +0530

    Refactor ContextProvider to handle delayed paragraph rendering with improved user experience

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -14,9 +14,12 @@ const ContextProvider = (props) => {
      const [ loading , setLoading ] = useState(false);
      const [ resultData , setResultData] = useState("");
 
-    const delayPara = (index,nextWord) => {
-
-    }
+     const delayPara = (index, nextWord) => {
+        setTimeout(() => {
+          setResultData((prev) => prev + nextWord);
+        }, 75 * index);
+      };
+      
 
 
     const onSent = async(prompt) => {
@@ -25,8 +28,34 @@ const ContextProvider = (props) => {
         setLoading(true);
         setShowResult(true);
         setRecentPrompt(input);
+
         const response =  await runChat(input);
-        setResultData(response);
+
+        let responseArray =  response.split("**");
+        let newResponse = "" ;
+        
+           for(let i = 0 ; i < responseArray.length ; i++){
+               if(i === 0 || i%2 !== 1){
+                   newResponse += responseArray[i];
+                }
+  
+               else {
+                   newResponse += "<b>"+responseArray[i]+"</b>";
+                }
+            }
+
+         let newResponse2 = newResponse.split("*").join("</br>");
+
+        // setResultData(newResponse);
+        // setResultData(newResponse2);
+
+        let newResponseArray = newResponse2.split(" ");
+
+             for(let i = 0 ; i < newResponseArray.length ; i++) {
+                    const nextWord = newResponseArray[i];
+                    delayPara(i,nextWord+" ");
+                 } 
+
         setLoading(false);
         setInput("");
     }
